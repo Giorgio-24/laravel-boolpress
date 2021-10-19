d2bf873dffad4938e392f95c45d02313d5b133cf
Giorgio Andrei <cipuandrei@libero.it>
2021-10-19 18:15:35 +0200

    admin routes added

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('guest.welcome');
+    return view('guest.home');
 });
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function () {
+
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+    //^Tutte le rotte protette dell'admin.
+});
