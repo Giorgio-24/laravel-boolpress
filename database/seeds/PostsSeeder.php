ea7952c8c7bc37a1aae9261dbcfc71f6bc99b0cc
Giorgio Andrei <cipuandrei@libero.it>
2021-10-22 18:01:21 +0200

    seeder modified and database filled

--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
 
 class PostsSeeder extends Seeder
@@ -13,8 +15,13 @@ class PostsSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        /* Qui sto selezionando la colonna id, pluck mi restituisce la collection con dentro l'array,
+ma per ciclare mi serve un array piatto, e quindi faccio toArray().  */
+        $categories_id = Category::select('id')->pluck('id')->toArray();
+
         for ($i = 0; $i < 50; $i++) {
             $newPost = new Post();
+            $newPost->category_id = Arr::random($categories_id);
             $newPost->title = $faker->text(50);
             $newPost->content = $faker->paragraphs(3, true);
             $newPost->image = $faker->imageUrl(640, 480);
